a0250806044729efc93315766b18261302342f62
Lucas Athié <luathie@gmail.com>
2025-09-04 15:04:35 -0300

    adicionando algumas funcionalidades sem estilização

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import react from 'react';
+import {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Bem vindo ao meu projeto</h1>
+function App(){
+
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+
+  useEffect(() => {
+    const tarefasLocalStorage = localStorage.getItem('@tarefa');
+
+    if(tarefasLocalStorage){
+      setTarefas(JSON.parse(tarefasLocalStorage))
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas))
+  }, [tarefas]);
+
+  function handleRegister(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando Usuário</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome da Tarefa: </label><br/>
+        <input 
+        placeholder = "Digite uma tarefa"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        /><br/>
+
+
+
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br/><br/>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
 
 export default App;
+
